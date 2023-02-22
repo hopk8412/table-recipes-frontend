9ea9f450805b96c12d4ad8f67536e8c265f8c989
hopk8412 <l1onh3art@protonmail.com>
2023-02-22 00:30:15 -0500

    very basic additions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script></script>
+
+<div>
+	<h1>Welcome to Table Recipes!</h1>
+	<p>
+		We hope you enjoy creating and viewing recipes from fellow home cooks, chefs, bakers, and more!
+	</p>
+    <input type="text" class="recipe-search" />
+	<button class="recipe-search-button">Find recipes</button>
+</div>
+
+<style>
+</style>
