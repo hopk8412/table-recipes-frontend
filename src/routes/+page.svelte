4d8091c57c4b072173e71fa50e1e349cd5050ebe
hopk8412 <l1onh3art@protonmail.com>
2023-02-22 00:34:18 -0500

    Small change to landing text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,8 @@
 <div>
 	<h1>Welcome to Table Recipes!</h1>
 	<p>
-		We hope you enjoy creating and viewing recipes from fellow home cooks, chefs, bakers, and more!
+        Enjoy a collection of community-driven recipes! Table Recipes wants to make creating and viewing recipes as simple and unintrusive as possible. 
+        No essay-length blog posts about the recipes, no intrusive advertisements or popups. Just a simple, clean interface so you can get back to what you enjoy the most!
 	</p>
     <input type="text" class="recipe-search" />
 	<button class="recipe-search-button">Find recipes</button>
